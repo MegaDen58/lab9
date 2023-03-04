f0ebee35a1c9941b5e48a57b97ca7e2046b876b3
MegaDen58 <e@w.com>
2023-03-04 12:24:02 +0300

    w

--- Program.cs
+++ Program.cs
@@ -1,86 +1,97 @@
 ﻿using System;
+using Students;
+using Readers;
+
+namespace Students
+{
+    class Student
+    {
+        string name;
+        int groupId;
+        private int age;
+
+        public Student(string name, int groupId, int age)
+        {
+            this.name = name;
+            this.groupId = groupId;
+            this.age = age;
+        }
+
+        public void Print() => Console.WriteLine($"Имя: {name}\nНомер группы: {groupId}\nВозраст: {age}");
+    }
+}
+
+namespace Readers
+{
+    class Reader
+    {
+        private string FIO;
+        int number;
+        string faculty;
+        private string birthday;
+
+        public Reader(string FIO, int number, string faculty, string birthday)
+        {
+            this.FIO = FIO; 
+            this.number = number;
+            this.faculty = faculty;
+            this.birthday= birthday;
+        }
+
+        public void takeBook(int bookCount) => Console.WriteLine($"{FIO} взял {bookCount} книги.");
+
+        public void takeBook(params string[] books)
+        {
+            string result = "";
+
+            foreach(string book in books)
+            {
+                result += $"{book},";
+            }
+
+            Console.WriteLine($"{FIO} взял книги: {result.TrimEnd(',')}.");
+        }
+
+        public void returnBook(int bookCount)
+        {
+            Console.WriteLine($"{FIO} вернул {bookCount} книги.");
+        }
+
+        public void returnBook(params string[] books)
+        {
+            string result = "";
+
+            foreach (string book in books)
+            {
+                result += $"{book},";
+            }
+
+            Console.WriteLine($"{FIO} вернул книги: {result.TrimEnd(',')}.");
+        }
+    }
+}
 
 public class Program
 {
     public static void Main()
     {
+        Student[] students = { new Student("Artem", 1, 15), 
+            new Student("Tom", 2, 16), 
+            new Student("Bob", 3, 17) 
+        };
+
+        Reader[] readers ={ new Reader("Петров В.В.", 113300, "Математический", "3 февраля 2000"), 
+            new Reader("Иванов Д.А.", 330011, "Философский", "17 ноября 20001"), 
+            new Reader("Кузнецов А.А.", 110033, "Математический", "3 декабря 2007") 
+        };
 
 
-        Student artem = new Student("Artem", 15, new int[] { 5, 5, 3, 4, 4 });
-        Student dima = new Student("Dima", 15, new int[] { 2, 5, 3, 2, 4 });
-        Student nikita = new Student("Nikita", 15, new int[] { 3, 3, 3, 3, 3 });
-        Student bob = new Student("Bob", 15, new int[] { 5, 5, 5, 5, 5 });
-        Student tom = new Student("Tom", 15, new int[] { 2, 5, 3, 3, 4 });
-        Student steve = new Student("Steve", 15, new int[] { 5, 5, 2, 4, 4 });
-        Student anton = new Student("Anton", 15, new int[] { 5, 5, 3, 3, 3 });
+        readers[2].takeBook("Приключения", "Словарь", "Энциклопедия");
+        readers[2].takeBook(3);
+        readers[2].returnBook("Приключения", "Словарь", "Энциклопедия");
+        readers[2].returnBook(4);
 
-        Student[] students = new Student[] { artem, dima, nikita, bob, tom, steve, anton };
-        Student[] newStudents = new Student[7];
+        students[1].Print();
 
-        int[] estimation = { artem.grade.Sum(), dima.grade.Sum(), nikita.grade.Sum(), 
-            bob.grade.Sum(), tom.grade.Sum(), steve.grade.Sum(), anton.grade.Sum() };
-
-        estimation = MyFunction(estimation);
-
-        for(int i = 0; i < students.Length; i++)
-        {
-            bool letsGo = true;
-            while (letsGo)
-            {
-                for(int k = 0; k < estimation.Length; k++)
-                {
-                    if (estimation[i] == students[k].grade.Sum())
-                    {
-                        newStudents[i] = students[k];
-                        letsGo = false;
-                    }
-                }
-            }
-        }
-
-        Console.WriteLine(ArrayToString(newStudents));
-    }
-
-    public static string ArrayToString(Student[] array)
-    {
-        string result = "";
-        for (int i = array.Length - 1; i >= 0; i--)
-        {
-            result += array[i].name + " ";
-        }
-        return result;
-    }
-
-    public static int[] MyFunction(int[] a)
-    {
-        for (int i = 1; i < a.Length; i++)
-        {
-            int k = a[i];
-            int j = i - 1;
-
-            while (j >= 0 && a[j] > k)
-            {
-                a[j + 1] = a[j];
-                a[j] = k;
-                j--;
-            }
-        }
-
-        return a;
-    }
-}
-
-
-
-public struct Student
-{
-    public string name;
-    public int groupId;
-    public int[] grade;
-    public Student(string name, int groupId, int[] grade)
-    {
-        this.name = name;
-        this.groupId = groupId;
-        this.grade = grade;
     }
 }
